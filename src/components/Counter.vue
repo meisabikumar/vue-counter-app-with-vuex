9aa2e653ce87d82a1a7262c75691aae64670eda1
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 01:00:06 +0530

    Update Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,12 +1,61 @@
 <template>
-    <div>Vue Counter</div>
+  <div>
+    <h1>Vuex Counter</h1>
+    <h1 class="counter">{{ getCounter }}</h1>
+    <button @click="subtractFromCounter(parseInt(value))">-</button>
+    <input type="number" v-model="value" />
+    <button @click="addToCounter(parseInt(value))">+</button>
+    <div>
+      <button class="btn" @click="addRandomNumber">Add by Random Number</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapMutations, mapGetters, mapState, mapActions } from "vuex";
 export default {
-  name: "Counter",
+  data() {
+    return {
+      value: 1,
+    };
+  },
+  computed: {
+    ...mapGetters(["getCounter"]),
+  },
+  methods: {
+    ...mapMutations(["addToCounter", "subtractFromCounter"]),
+    ...mapActions(["addRandomNumber"]),
+  },
+  mounted() {
+    console.log(this.$store.state);
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.counter {
+  font-size: 5rem;
+}
+button {
+  border-radius: 100%;
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  font-weight: 700;
+  cursor: pointer;
+}
+input {
+  padding: 0.4rem;
+  margin: 0 0.5rem;
+  text-align: center;
+}
+
+.btn {
+  border-radius: 0.5rem;
+  width: auto;
+  background: #41b983;
+  margin-top: 1rem;
+  color: white;
+  cursor: pointer;
+}
 </style>
