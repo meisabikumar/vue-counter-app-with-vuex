5d28b977701e95b84b4a31fc71e126bf67f2937a
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 00:14:48 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <img alt="Vue logo" src="./assets/logo.png">
 
 </template>
 
@@ -14,5 +15,12 @@ export default {
 </script>
 
 <style>
-
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
