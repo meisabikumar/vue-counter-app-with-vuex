39a45d81947f11de2799e3c3d94f0f88d769d1a1
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 00:28:20 +0530

    counter component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Counter />
 </template>
 
 <script>
-
+import Counter from './components/Counter.vue';
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
-  }
-}
+    Counter
+  },
+};
 </script>
 
 <style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Vue Counter</div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+};
+</script>
+
+<style>
+</style>
