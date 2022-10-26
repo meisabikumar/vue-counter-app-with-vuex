30d6ef6805d7f2192f9356634f8999256d6c4a11
Abhishek Kumar <58994412+meisabikumar@users.noreply.github.com>
2022-10-27 00:59:59 +0530

    Update counter.js

--- src/store/modules/counter.js
+++ src/store/modules/counter.js
@@ -1,9 +1,38 @@
+import axios from "axios";
+
 export default {
   state: {
     counter: 0,
     history: [0],
   },
-  getters: {},
-  mutations: {},
-  actions: {},
+  getters: {
+    getCounter: (state) => state.counter,
+    getHistory: (state) => state.history,
+    activeIndexes: (state) => (payload) => {
+      let indexes = [];
+      state.history.forEach((number, index) => {
+        if (number === payload) {
+          indexes.push(index);
+        }
+      });
+      return indexes;
+    },
+  },
+  mutations: {
+    addToCounter(state, payload) {
+      state.counter += payload;
+      state.history.push(state.counter);
+    },
+    subtractFromCounter(state, payload) {
+      state.counter -= payload;
+      state.history.push(state.counter);
+    },
+  },
+  actions: {
+    async addRandomNumber(context) {
+      let data = await axios.get("https://www.random.org/integers/?num=1&min=-1000&max=1000&col=1&base=10&format=plain&rnd=new");
+      console.log( data.data);
+      context.commit("addToCounter", Math.abs(data.data));
+    },
+  },
 };
